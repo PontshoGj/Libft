536999b61127adf7ecc5fed1b1e4645f7565f0e8
Pontsho MOGWERE <pmogwere@c5r5s7.wethinkcode.co.za>
2019-08-16 14:18:08 +0200

    adding ft_itoa_base and ft_strspn

---
+++ ft_itoa_base.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmogwere <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/16 14:14:17 by pmogwere          #+#    #+#             */
+/*   Updated: 2019/08/16 14:16:02 by pmogwere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_itoa_base(int value, int base)
+{
+	char	*s;
+	char	*str;
+	int		neg;
+	int		len;
+
+	s = "0123456789ABCDEF";
+	len = ft_intlen(value);
+	neg = 1;
+	if (value < 0)
+	{
+		neg *= -1;
+		value *= -1;
+		(base == 10) ? len++ : 0;
+	}
+	str = (char *)malloc(sizeof(char) * len);
+	str[len] = 0;
+	while (value)
+	{
+		str[--len] = s[value % base];
+		value /= base;
+	}
+	(neg < 0 && base == 10) ? str[0] = '-' : 0;
+	return (str);
+}
